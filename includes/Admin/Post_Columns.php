3dd1c95d6e5d9a55bc5c48752ac0856d9cc72c0b
Robiul Islam <therobiulislam12@gmail.com>
2024-07-11 23:58:55 +0600

    3 extra post columns added done on admin panel

--- includes/Admin/Post_Columns.php
+++ includes/Admin/Post_Columns.php
@@ -4,10 +4,36 @@ namespace Robiul\CPT\Admin;
 
 class Post_Columns {
     public function __construct() {
-        add_filter( 'manage_books_posts_columns', array( $this, 'robi_cpt_books_post_type_columns' ) );
+        add_filter( 'manage_books_posts_columns', array( $this, 'robi_cpt_books_columns' ) );
     }
 
-    public function robi_cpt_books_post_type_columns( $post_columns ) {
-        // print_r( $post_columns );
+    public function robi_cpt_books_columns( $post_columns ) {
+
+        // create a new array
+        $new_columns = [];
+
+        // loop the post_columns array
+        foreach($post_columns as $key=>$column){
+
+            if ( 'cb' == $key ) {
+				$new_columns[ $key ] = $column;
+				$new_columns['book_image'] = 'Thumbnail';
+				continue;
+			}
+
+            if ( 'title' == $key ) {
+				$new_columns[ $key ] = $column;
+				$new_columns['book_author'] = 'Author';
+
+                $new_columns[ $key ] = $column;
+				$new_columns['book_price'] = 'Price';
+
+				continue;
+			}
+
+            $new_columns[ $key ] = $column;
+        }
+        
+        return $new_columns;
     }
 }
