cfe61aa95923b83a92508951a17e249584c3efe4
Robiul Islam <therobiulislam12@gmail.com>
2024-07-13 10:05:59 +0600

    Make columns sortable

--- includes/Admin/Post_Columns.php
+++ includes/Admin/Post_Columns.php
@@ -4,7 +4,10 @@ namespace Robiul\CPT\Admin;
 
 class Post_Columns {
     public function __construct() {
+
         add_filter( 'manage_books_posts_columns', array( $this, 'robi_cpt_books_columns' ) );
+        add_action( 'manage_books_posts_custom_column', array($this, 'robi_custom_book_column'), 10, 2 );
+        add_filter( 'manage_edit-books_sortable_columns', array($this, 'robi_column_sortable'), 10, 1);
     }
 
     public function robi_cpt_books_columns( $post_columns ) {
@@ -13,27 +16,73 @@ class Post_Columns {
         $new_columns = [];
 
         // loop the post_columns array
-        foreach($post_columns as $key=>$column){
+        foreach ( $post_columns as $key => $column ) {
 
             if ( 'cb' == $key ) {
-				$new_columns[ $key ] = $column;
-				$new_columns['book_image'] = 'Thumbnail';
-				continue;
-			}
+                $new_columns[$key] = $column;
+                $new_columns['book_image'] = 'Thumbnail';
+                continue;
+            }
 
             if ( 'title' == $key ) {
-				$new_columns[ $key ] = $column;
-				$new_columns['book_author'] = 'Author';
+                $new_columns[$key] = $column;
 
-                $new_columns[ $key ] = $column;
-				$new_columns['book_price'] = 'Price';
+                $new_columns['book_author'] = 'Author';
+                $new_columns['book_price'] = 'Price';
+                $new_columns['number_of_chapter'] = 'Chapters';
 
-				continue;
-			}
+                continue;
+            }
 
-            $new_columns[ $key ] = $column;
+            $new_columns[$key] = $column;
         }
-        
+
         return $new_columns;
     }
+
+    public function robi_custom_book_column( $column, $post_id ) {
+        if ( 'book_image' == $column ) {
+			if ( has_post_thumbnail( $post_id ) ) {
+				echo get_the_post_thumbnail($post_id, 'thumbnail');
+			} else {
+				echo 'No Image';
+			}
+		}
+
+        if('book_author' === $column){
+
+            global $post;
+			// get previous count
+			$author = get_post_meta( $post->ID, 'author', true );
+
+            echo $author;
+        }
+
+        if('book_price' === $column){
+
+            global $post;
+			// get previous count
+			$book_price = get_post_meta( $post->ID, 'book_price', true );
+
+            echo $book_price;
+        }
+        
+        if('number_of_chapter' === $column){
+
+            global $post;
+			// get previous count
+			$chapters = get_post_meta( $post->ID, 'numbers_of_chapter', true );
+
+            echo $chapters;
+        }
+    }
+
+    public function robi_column_sortable($cols){
+        $cols['book_author'] = 'book_author';
+        $cols['book_price'] = 'book_price';
+        $cols['number_of_chapter'] = 'number_of_chapter';
+
+        return $cols;
+    }
+
 }
